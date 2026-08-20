d0fc7a88de8c736346d27a04d13dd037f00e10d0
khimrous <popopes355@gmail.com>
2026-08-20 17:20:41 +0700

    force update vite base path to repository name

--- Food-web/vite.config.js
+++ Food-web/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [vue()],
+  base: '/WCT-FINAL-PROJECT/' // 👈 ត្រូវដូរមកដាក់បែបនេះ ដើម្បីឱ្យវាស្គាល់ទាំងឈ្មោះ Repository និង Folder ខាងក្នុង
 })
